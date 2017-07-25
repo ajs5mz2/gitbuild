ae20b7494e45169b02c1cb0cfef2492df21ea901
asmith8 <asmith8@W7M-352045.carefirst.com>
2017-07-25 13:19:30 -0400

    Last Change to component

--- gitBuildDemo/src/com/carefirst/arms/gitdemo/PrintDemo.java
+++ gitBuildDemo/src/com/carefirst/arms/gitdemo/PrintDemo.java
@@ -4,7 +4,7 @@ public class PrintDemo {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("This is a Jenkins Build Demo 3");
+		System.out.println("This is a Jenkins Build Demo 4");
 	}
 
 }
