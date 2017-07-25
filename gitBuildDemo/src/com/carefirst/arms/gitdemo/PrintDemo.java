8184958262a7c17d4d666809a6ffb6dde8899858
asmith8 <asmith8@W7M-352045.carefirst.com>
2017-07-25 12:41:20 -0400

    Commit by Art for gitTest

---
+++ gitBuildDemo/src/com/carefirst/arms/gitdemo/PrintDemo.java
@@ -0,0 +1,10 @@
+package com.carefirst.arms.gitdemo;
+
+public class PrintDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("This is a Jenkins Build Demo 3");
+	}
+
+}
